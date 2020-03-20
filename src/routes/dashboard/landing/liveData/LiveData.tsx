e19817d7f6bad85f801e6ceeb142ba950180a612
shrestharosy028 <rosy@fusemachines.com>
2020-03-20 19:17:52 +0545

    [COV-23] : Fetch districts in live data table

--- src/routes/dashboard/landing/liveData/LiveData.tsx
+++ src/routes/dashboard/landing/liveData/LiveData.tsx
@@ -4,17 +4,30 @@ import { Col } from 'react-bootstrap';
 import LiveDataTable from './Table/LiveDataTable';
 import LiveDataFilter from './Table/LiveDataFilter';
 import { ILiveData, fetchLiveDataAPI } from 'src/services/liveData';
+import { fetchDistrictListAPI, IFetchDistrictListAPIResponse } from 'src/services/contacts';
+import { ProvinceOptions } from 'src/constants/options';
 
 export interface ILiveDataTableContext {
   isLoaded: boolean;
   liveDataList: Array<ILiveData>;
 }
 
+interface ILiveDataFilters {
+  province: string;
+  district: string;
+}
+
 export const LiveDataTableContext = React.createContext({} as ILiveDataTableContext);
+
+const initialLiveDataFiltersState = {
+  province: ProvinceOptions[2].value,
+  district: 'Kathmandu'
+};
 
 const LiveData: FC<{}> = () => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [liveDataList, setLiveDataList] = useState<Array<ILiveData>>([]);
+  const [filters, setFilters] = useState<ILiveDataFilters>(initialLiveDataFiltersState);
 
   useEffect(() => {
     fetchLiveData();
@@ -29,6 +42,20 @@ const LiveData: FC<{}> = () => {
       console.log(error);
     } finally {
       setIsLoaded(true);
+    }
+  };
+
+  const fetchDistrictsByProvince = async () => {
+    try {
+      const response: IFetchDistrictListAPIResponse = await fetchDistrictListAPI(filters.province);
+      // setDistrictList([
+      //   {
+      //     label: response.name,
+      //     value: response.name
+      //   }
+      // ]);
+    } catch (error) {
+      console.log(error);
     }
   };
 
