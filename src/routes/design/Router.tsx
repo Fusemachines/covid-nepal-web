588a4545ff887032b76f36a2d7b6abe4fd229816
sanishkarmacharya <sanish@fusemachines.com>
2020-03-20 10:16:20 +0545

    symptoms page completed

--- src/routes/design/Router.tsx
+++ src/routes/design/Router.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Landing from './Home';
+import Symptoms from './Symptoms';
 
 const DesignRoutes: React.FC = () => (
   <Switch>
     <Route exact path={'/design'} component={Landing} />
+    <Route exact path={'/symptoms'} component={Symptoms} />
   </Switch>
 );
 
