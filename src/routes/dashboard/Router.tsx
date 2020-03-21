9110d2bebc9673c520b75a069f93711dc1363a5c
jagatmachines <jagatjyoti@fusemachines.com>
2020-03-22 01:26:11 +0545

    google analytics and favicon

--- src/routes/dashboard/Router.tsx
+++ src/routes/dashboard/Router.tsx
@@ -6,13 +6,14 @@ import Landing from 'src/routes/dashboard/landing';
 import Symptoms from 'src/routes/dashboard/symptoms';
 import HospitalDetails from 'src/routes/dashboard/hospitalDetails';
 import About from 'src/routes/dashboard/about';
+import withTracker from 'src/utils/googleAnalytics';
 
 const DashboardRouter: FC<{}> = () => (
   <Switch>
-    <Route exact path={routes.DASHBOARD} component={Landing} />
-    <Route path={routes.SYMPTOMS} component={Symptoms} />
-    <Route path={routes.HOSPITAL_DETAIL} component={HospitalDetails} />
-    <Route path={routes.ABOUT} component={About} />
+    <Route exact path={routes.DASHBOARD} component={withTracker(Landing)} />
+    <Route path={routes.SYMPTOMS} component={withTracker(Symptoms)} />
+    <Route path={routes.HOSPITAL_DETAIL} component={withTracker(HospitalDetails)} />
+    <Route path={routes.ABOUT} component={withTracker(About)} />
   </Switch>
 );
 
