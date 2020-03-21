f77c0008f8cbcb29a53b6b26d6eeca2bac958a67
pragyakarfuse <pragyakar@fusemachines.com>
2020-03-21 18:53:03 +0545

    [COV-33] : Added hospital details page

--- src/routes/dashboard/hospitalDetails/DetailsBox/DetailsBox.tsx
+++ src/routes/dashboard/hospitalDetails/DetailsBox/DetailsBox.tsx
@@ -1,51 +1,58 @@
 import React, { FC } from 'react';
 import GreenContactBadge from 'src/components/Badges/GreenContactBadge';
+import { IHospital } from 'src/services/hospitals';
 
-// TODO:pragyakar update while backend integration
-interface IDetailsBoxProps {}
+interface IDetailsBoxProps {
+  hospital: IHospital;
+}
 
 const DetailsBox: FC<IDetailsBoxProps> = props => {
+  const { hospital } = props;
   return (
     <div className="rounded bg-bluelight overflow-hidden">
       <div className="p-4 text-white">
-        <div className="hospital-title">Teku Hospital</div>
+        <div className="hospital-title">{hospital.name}</div>
         {/* <div className="my-3">
                 <img src="" className="img-responsive" alt="hospital" />
               </div> */}
 
         <ul className="hospitalinfo-list">
           <li>
-            <span className="text-secondary">Address:</span> <span className="ml-2 text-success">Teku, Kathmandu</span>
+            <span className="text-secondary">Address:</span>{' '}
+            <span className="ml-2 text-success">
+              {hospital.location}, {hospital.district}
+            </span>
           </li>
           <li>
             <span className="text-secondary">Open hours:</span>
-            <span className="mx-2">8 AM - 3 PM</span>
-            <span className="ml-3">SUN - FRI</span>
+            <span className="mx-2">
+              {hospital.availableTime[0]} AM - {hospital.availableTime[1]}PM
+            </span>
+            <span className="ml-3">{hospital.openDays}</span>
           </li>
           <li>
             <span className="text-secondary">Beds for COVID-19 patients :</span>
-            <span className="ml-2 h6 mb-0 text-warning font-weight-bold">50 / 120</span>
+            <span className="ml-2 h6 mb-0 text-warning font-weight-bold">
+              {hospital.availableBeds} / {hospital.totalBeds}
+            </span>
           </li>
           <li>
             <span className="text-secondary">Contact :</span>
-            <GreenContactBadge contactNumber={'01-1234567'} />
-            <GreenContactBadge contactNumber={'01-1234567'} />
+            {hospital.contact.map(contact => (
+              <GreenContactBadge contactNumber={contact} />
+            ))}
           </li>
           <li>
             <span className="text-secondary">Hosptial Type :</span>
-            <span className="ml-2">Government</span>
+            <span className="ml-2">{hospital.hospitalType}</span>
           </li>
         </ul>
       </div>
-
-      <div className="maps">
-        <iframe
-          title="location"
-          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14130.828627230478!2d85.3069413!3d27.6954445!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xbd8552665e65d95b!2sTeku%20Hospital%20(Shukraraaj%20Tropical%20%26%20Infectious%20Disease%20Hospital)!5e0!3m2!1sen!2snp!4v1584778123055!5m2!1sen!2snp"
-          width="400"
-          height="300"
-        ></iframe>
-      </div>
+      {hospital.mapLink && (
+        <div className="maps">
+          <iframe title="location" src={hospital.mapLink} width="400" height="300"></iframe>
+        </div>
+      )}
     </div>
   );
 };
