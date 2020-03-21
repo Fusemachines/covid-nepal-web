1270f4006ba41e676ee986fb7e5c470993a9965b
shrestharosy028 <rosy@fusemachines.com>
2020-03-21 16:38:48 +0545

    [COV-21] : Show updated time of covid case and add refresh button

--- src/routes/dashboard/landing/covidCases/CovidCases.tsx
+++ src/routes/dashboard/landing/covidCases/CovidCases.tsx
@@ -4,21 +4,35 @@ import { Row, Col } from 'react-bootstrap';
 import NepalCovidCases from './NepalCovidCases';
 import GlobalCovidCases from './GlobalCovidCases';
 import { fetchCovidCasesCountsAPI, ICovidCasesCounts } from 'src/services/covidCases';
+import RefreshIcon from 'src/components/Icons/RefreshIcon';
 
 const CovidCases = () => {
   const [covidCasesCounts, setCovidCasesCounts] = useState<ICovidCasesCounts | null>(null);
+  const [timeOfFetch, setTimeOfFetch] = useState<Date>(new Date());
+  const [updatedTime, setUpdatedTime] = useState(0);
 
   useEffect(() => {
     getVirusCounts();
+    window.setInterval(getUpdatedTime, 30000);
+    return () => {
+      clearInterval();
+    };
   }, []);
 
   const getVirusCounts = async () => {
     try {
+      setTimeOfFetch(new Date());
       const response = await fetchCovidCasesCountsAPI();
       setCovidCasesCounts(response);
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const getUpdatedTime = () => {
+    const currentTime = new Date();
+    const interval = currentTime.getMinutes() - timeOfFetch.getMinutes();
+    setUpdatedTime(interval);
   };
 
   return (
@@ -28,7 +42,12 @@ const CovidCases = () => {
           <div className="mb-3 border-bottom pb-2">
             <div className="d-inline-block">
               <div className="h5 mb-0 font-weight-bold">Covid-19 Cases</div>
-              <small>Updated 1 minutes ago</small>
+              <small>
+                {updatedTime > 0 ? `Updated ${updatedTime} minutes ago` : `Updated a while ago`}
+                <i className="ml-2 pointer" onClick={() => getVirusCounts()}>
+                  <RefreshIcon />
+                </i>
+              </small>
             </div>
           </div>
           <div className="clearfix"></div>
