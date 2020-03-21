035c2a79b0e13bad3ca6206dd7c898a72407679e
pragyakarfuse <pragyakar@fusemachines.com>
2020-03-22 00:46:47 +0545

    [FIX] : Changed twitter handle to coivdnepalorg

--- src/routes/dashboard/landing/latestNews/LatestNews.tsx
+++ src/routes/dashboard/landing/latestNews/LatestNews.tsx
@@ -9,7 +9,7 @@ const LatestNews = () => {
 
   return (
     <div className="twitter-embed">
-      <a className="twitter-timeline" href="https://twitter.com/WHO?ref_src=twsrc%5Etfw">
+      <a className="twitter-timeline" href="https://twitter.com/covidnepalorg?ref_src=twsrc%5Etfw">
         COVID-19 Nepal News
       </a>
     </div>
