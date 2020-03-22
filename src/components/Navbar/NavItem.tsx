b9cc751c24b0006f7998fb8dc857cf6ee13eb7a5
jagatmachines <jagatjyoti@fusemachines.com>
2020-03-22 14:32:02 +0545

    mobile navLink refetch issue solved

--- src/components/Navbar/NavItem.tsx
+++ src/components/Navbar/NavItem.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { Nav } from 'react-bootstrap';
-// import { NavLink, useHistory } from 'react-router-dom';
+import { /* NavLink, */ useHistory } from 'react-router-dom';
 
 interface INavItemProps {
   to: string;
@@ -10,12 +10,21 @@ interface INavItemProps {
 }
 
 const NavItem: FC<INavItemProps> = props => {
-  // const history = useHistory();
+  const history = useHistory();
 
   const { to, title, active /* , exact = true */ } = props;
 
+  const onSelect = (eventKey: any, e: any) => {
+    e.preventDefault();
+    history.push(to);
+  };
+
   return (
-    <Nav.Link href={to} /* exact={exact} to={to} */ className={`nav-link ${active ? 'active' : ''}`}>
+    <Nav.Link
+      href={to}
+      onSelect={onSelect}
+      /* exact={exact} to={to} */ className={`nav-link ${active ? 'active' : ''}`}
+    >
       {title}
     </Nav.Link>
   );
