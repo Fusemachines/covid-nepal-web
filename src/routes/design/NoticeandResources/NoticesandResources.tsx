5e4ddc1308acca61b74c70b0c28f674631c24a78
bibekkc1250 <bibek@fusemachines.com>
2020-03-23 13:04:15 +0545

    Resource section complete

--- src/routes/design/NoticeandResources/NoticesandResources.tsx
+++ src/routes/design/NoticeandResources/NoticesandResources.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import Menu from '../Navbar';
-import { Row, Col, Tab, Nav } from 'react-bootstrap';
+import { Row, Col, Tab, Nav, Media } from 'react-bootstrap';
 
 const NoticesandResources = () => (
   <>
     <Menu />
 
     <div className="container">
-      <Row>
-        <Col lg={10}>
+      <Row className="mt-5 bg-bluelight semi-rounded">
+        <Col lg={10} className="pt-4 pb-4">
           <Tab.Container id="left-tabs-example" defaultActiveKey="resources">
-            <Nav variant="tabs">
+            <Nav variant="tabs" className="">
               <Nav.Item>
                 <Nav.Link eventKey="notice">Government Notices</Nav.Link>
               </Nav.Item>
@@ -25,7 +25,46 @@ const NoticesandResources = () => (
               </Tab.Pane>
 
               <Tab.Pane eventKey="resources">
-                <div className="p-4">Resources</div>
+                <ul className="resources__list">
+                  <li>
+                    <a href="https://www.unicef.org/nepal/stories/novel-coronavirus" className="p-3">
+                      <Media>
+                        <img
+                          width={80}
+                          height={80}
+                          className="mr-3"
+                          src="/images/protect_yourself.png"
+                          alt="Generic placeholder"
+                        />
+                        <Media.Body>
+                          <h5 className="resources__title">How to protect yourself and others against COVID-19</h5>
+                          <span className="resources__link">
+                            https://www.unicef.org/nepal/stories/novel-coronavirus
+                          </span>
+                        </Media.Body>
+                      </Media>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.unicef.org/nepal/join-fight-against-misinformation" className="p-3">
+                      <Media>
+                        <img
+                          width={80}
+                          height={80}
+                          className="mr-3"
+                          src="/images/fight_misinformation.png"
+                          alt="Generic placeholder"
+                        />
+                        <Media.Body>
+                          <h5 className="resources__title">Join the fight against misinformation</h5>
+                          <span className="resources__link">
+                            https://www.unicef.org/nepal/join-fight-against-misinformation
+                          </span>
+                        </Media.Body>
+                      </Media>
+                    </a>
+                  </li>
+                </ul>
               </Tab.Pane>
             </Tab.Content>
           </Tab.Container>
