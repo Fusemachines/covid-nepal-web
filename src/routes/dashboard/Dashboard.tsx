39166316ebfe82545648a8b8105717599a859d06
pragyakarfuse <pragyakar@fusemachines.com>
2020-03-23 16:59:41 +0545

    [Fix] : Moved Contact inside Navbar to prevent rerender

--- src/routes/dashboard/Dashboard.tsx
+++ src/routes/dashboard/Dashboard.tsx
@@ -5,24 +5,9 @@ import DashboardRouter from 'src/routes/dashboard/Router';
 import Contacts from 'src/routes/dashboard/contacts';
 
 const Dashboard: FC<{}> = () => {
-  const [isSidebarVisible, setIsSidebarVisible] = useState(false);
-
-  const toggleEmergencyContact = () => {
-    setIsSidebarVisible(!isSidebarVisible);
-  };
-
-  useEffect(() => {
-    if (isSidebarVisible) {
-      document.getElementsByTagName('body')[0].classList.add('modal-open');
-    } else {
-      document.getElementsByTagName('body')[0].classList.remove('modal-open');
-    }
-  }, [isSidebarVisible]);
-
   return (
     <>
-      <Navbar toggleSidebar={toggleEmergencyContact} />
-      <Contacts visibility={isSidebarVisible} toggleContacts={toggleEmergencyContact} />
+      <Navbar />
       <DashboardRouter />
     </>
   );
