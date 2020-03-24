45ae6e59b9bf22e2be04891a5eec9ea383c295d4
sanishkarmacharya <sanish@fusemachines.com>
2020-03-24 11:09:09 +0545

    flag png change with svg

--- src/components/Navbar/LanguageSelectCommingSoon.tsx
+++ src/components/Navbar/LanguageSelectCommingSoon.tsx
@@ -15,12 +15,12 @@ const LanguageSelectCommingSoon: React.FC<any> = ({ isMobile }) => {
       <div className={`lang ${isMobile ? 'mobile-flag' : 'menu-flag'}`}>
         <label htmlFor="np-lang">
           <input type="radio" id="np-lang" onClick={showPopupMessage} name="language" value="ne" />
-          <img src="/images/nepal.png" className="mx-1" /> {isMobile ? '' : navBar.NEP}
+          <img src="/images/nepal.svg" className="mx-1" /> {isMobile ? '' : navBar.NEP}
         </label>
 
         <label htmlFor="en-lang" className={'active'}>
           <input type="radio" id="en-lang" name="language" value="en" checked={true} />
-          {isMobile ? '' : navBar.ENG} <img src="/images/english.png" className="mx-1" />
+          {isMobile ? '' : navBar.ENG} <img src="/images/english.svg" className="mx-1" />
         </label>
       </div>
       {showModal ? (
