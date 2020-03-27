4ff1d80c9c32f0a69df3ee2ceb65081b6e161b6c
sanishkarmacharya <sanish@fusemachines.com>
2020-03-27 18:02:14 +0545

    FAQ content added

--- src/routes/dashboard/faq/FAQ.tsx
+++ src/routes/dashboard/faq/FAQ.tsx
@@ -22,26 +22,26 @@ const JoinUs = () => {
             <h1 className="text-center font-weight-bold">{t(lo.nav_FAQ)}</h1>
           </Col>
 
-          {/* <Col sm="12" className="my-3">
+          <Col sm="12" className="my-3">
             <div className="rounded bg-bluelight p-4">
-              <ul className="numc">
-                <li className="mb-4">
+              {/* <ul className="numc">
+                <li className="mb-5">
                   <div className="font-18 mb-2">{t(lo.faq_q1_ques)}</div>
                   <p>{t(lo.faq_q1_ans_1)}</p>
                   <p>{t(lo.faq_q1_ans_2)}</p>
                 </li>
 
-                <li className="mb-4">
+                <li className="mb-5">
                   <div className="font-18 mb-2">{t(lo.faq_q2_ques)}</div>
                   <p>{t(lo.faq_q2_ans)}</p>
                 </li>
 
-                <li className="mb-4">
+                <li className="mb-5">
                   <div className="font-18 mb-2">{t(lo.faq_q3_ques)}</div>
                   <p>{t(lo.faq_q3_ans)}</p>
                 </li>
 
-                <li className="mb-4">
+                <li className="mb-5">
                   <div className="font-18 mb-2">{t(lo.faq_q4_ques)}</div>
                   <p>{t(lo.faq_q4_ans)}</p>
                 </li>
@@ -50,9 +50,126 @@ const JoinUs = () => {
                   <div className="font-18 mb-2">{t(lo.faq_q5_ques)}</div>
                   <p>{t(lo.faq_q5_ans)}</p>
                 </li>
+              </ul> */}
+
+              <ul className="numc">
+                
+                <li className="mb-5">
+                  <div className="font-18 mb-2">What is a coronavirus?</div>
+                  <p>Coronaviruses are a large family of viruses that may cause illness in animals or humans.  In humans, several coronaviruses are known to cause respiratory infections. The most recently discovered coronavirus causes coronavirus disease COVID-19.</p>
+                </li>
+                
+                <li className="mb-5">
+                  <div className="font-18 mb-2">What is COVID-19?</div>
+                  <p>COVID-19 is an infectious disease caused by the most recently discovered coronavirus. This new virus and disease were unknown before the outbreak began in Wuhan, China, in December 2019.</p>
+                </li>
+                
+                <li className="mb-5">
+                  <div className="font-18 mb-2">What are the symptoms of COVID-19?</div>
+                  <p>The most common symptoms of COVID-19 are fever, tiredness, and dry cough. Some patients may have aches and pains, nasal congestion, runny nose, sore throat or diarrhea. These symptoms are usually mild and begin gradually. Some people become infected but don’t develop any symptoms and don't feel unwell.</p>
+                </li>
+                
+                <li className="mb-5">
+                  <div className="font-18 mb-2">How does the COVID-19 disease transmit from person to person?</div>
+                  <p>The disease can spread from person to person if they breathe in the small droplets containing the virus when that person with COVID-19 coughs or exhales. These droplets land on objects and surfaces around the person. Other people then catch COVID-19 by touching these objects or surfaces, then touching their eyes, nose or mouth.</p>
+                </li>
+                
+                <li className="mb-5">
+                  <div className="font-18 mb-2">Can the virus that causes COVID-19 be transmitted through the air?</div>
+                  <p>Studies to date suggest that the virus that causes COVID-19 is mainly transmitted through contact with respiratory droplets rather than through the air.</p>
+                </li>
+                
+                <li className="mb-5">
+                  <div className="font-18 mb-2">Can COVID-19 be caught from a person who has no symptoms?</div>
+                  <p>It is possible to catch COVID-19 from someone who has, for example, just a mild cough and does not feel ill.</p>
+                </li>
+                
+                <li className="mb-5">
+                  <div className="font-18 mb-2">What can I do to protect myself and prevent the spread of disease?</div>
+                  <p>You can reduce your chances of being infected or spreading COVID-19 by taking some simple precautions:</p>
+                  <ul className="disc">
+                    <li className="mb-2">Regularly and thoroughly clean your hands for at least 20 seconds with an alcohol-based hand rub or wash them with soap and water.</li>
+                    <li className="mb-2">Maintain at least 1 metre (3 feet) distance between yourself and anyone who is coughing or sneezing.</li>
+                    <li className="mb-2">Avoid touching eyes, nose and mouth. Hands touch many surfaces and can pick up viruses. Once contaminated, hands can transfer the virus to your eyes, nose or mouth. From there, the virus can enter your body and can make you sick.</li>
+                    <li className="mb-2">Cover your mouth and nose with your bent elbow or tissue when you cough or sneeze. Then dispose of the used tissue immediately.</li>
+                    <li className="mb-2">Stay home if you feel unwell. If you have a fever, cough and difficulty breathing, seek medical attention and call in advance. Follow the directions of your local health authority.</li>
+
+
+                    <li className="mb-2">Keep up to date on the latest COVID-19 hotspots (cities or local areas where COVID-19 is spreading widely). If possible, avoid traveling to places  – especially if you are an older person or have diabetes, heart or lung disease.</li>
+                  </ul>
+                  Why? You have a higher chance of catching COVID-19 in one of these areas.
+                </li>
+                
+                <li className="mb-5">
+                  <div className="font-18 mb-2">Are antibiotics effective in preventing or treating the COVID-19?</div>
+                  <p>No. Antibiotics do not work against viruses, they only work on bacterial infections.</p>
+                </li>
+                
+                <li className="mb-5">
+                  <div className="font-18 mb-2">Are there any medicines to cure COVID-19?</div>
+                  <p>There is no evidence that current medicine can prevent or cure the disease. There are several ongoing clinical trials that include both western and traditional medicines.</p>
+                </li>
+                
+                <li className="mb-5">
+                  <div className="font-18 mb-2">Will wearing a mask protect me from the COVID-19?</div>
+                  <p>Not really. Only wear a mask if you are ill with COVID-19 symptoms (especially coughing) or looking after someone who may have COVID-19. The most effective ways to protect yourself and others against COVID-19 are to frequently clean your hands, cover your cough with the bend of elbow or tissue and maintain a distance of at least 1 meter (3 feet) from people.</p>
+                </li>
+                
+                <li className="mb-5">
+                  <div className="font-18 mb-2">Can humans become infected with the COVID-19 from an animal source?</div>
+                  <p>It’s possible. Handle raw meat, milk or animal organs with care to avoid contamination of uncooked foods and avoid consuming raw or undercooked animal products.</p>
+                </li>
+                
+                <li className="mb-5">
+                  <div className="font-18 mb-2">Can I catch COVID-19 from my pet?</div>
+                  <p>There is no evidence that a dog, cat or any pet can transmit COVID-19.</p>
+                </li>
+                
+                <li className="mb-5">
+                  <div className="font-18 mb-2">How long does the virus survive on surfaces?</div>
+                  <p>Studies suggest that coronaviruses (including preliminary information on the COVID-19 virus) may persist on surfaces for a few hours or up to several days.</p>
+                </li>
+                
+                <li className="mb-5">
+                  <div className="font-18 mb-2">Will warm weather stop the outbreak?</div>
+                  <p>At this time, it is not known whether the spread of COVID-19 will decrease when weather becomes warmer.</p>
+                </li>
+                
+                <li className="mb-5">
+                  <div className="font-18 mb-2">How can pregnant women protect themselves from getting COVID-19?</div>
+                  <p>Pregnant women should do the same things as the general public to avoid infection. You can help stop the spread of COVID-19 by taking these actions:</p>
+                  <ul className="disc">
+                    <li className="mb-2">Cover your cough (using your elbow is a good technique).</li>
+                    <li className="mb-2">Avoid people who are sick.</li>
+                    <li className="mb-2">Clean your hands often using soap and water or alcohol-based hand sanitizer.</li>
+                  </ul>
+                </li>
+                
+                <li className="mb-5">
+                  <div className="font-18 mb-2">Can COVID-19 be passed from a pregnant woman to the fetus or newborn?</div>
+                  <p>We still do not know if a pregnant woman with COVID-19 can pass the virus that causes COVID-19 to her fetus or baby during pregnancy or delivery.</p>
+                </li>
+                
+                <li className="mb-5">
+                  <div className="font-18 mb-2">Can the COVID-19 virus spread through drinking water?</div>
+                  <p>The COVID-19 virus has not been detected in drinking water. Conventional water treatment methods that use filtration and disinfection, such as those in most municipal drinking water systems, should remove or inactivate the virus that causes COVID-19.</p>
+                </li>
+                
+                <li className="mb-5">
+                  <div className="font-18 mb-2">Is the COVID-19 virus found in feces?</div>
+                  <p>The virus that causes COVID-19 has been detected in the feces of some patients diagnosed with COVID-19. The amount of virus released from the body (shed) in stool, how long the virus is shed, and whether the virus in the stool is infectious are not known.</p>
+                  <p>The risk of transmission of COVID-19 from the feces of an infected person is also unknown.</p>
+                </li>
+
               </ul>
+              
+
+              <div className="h5">Sources</div>
+              <div><a className="text-white" href="https://www.who.int/news-room/q-a-detail/q-a-coronaviruses" target="_blank">https://www.who.int/news-room/q-a-detail/q-a-coronaviruses</a></div>
+              <div><a className="text-white mb-4" href="https://www.cdc.gov/coronavirus/2019-ncov/faq.html" target="_blank">https://www.cdc.gov/coronavirus/2019-ncov/faq.html</a></div>
+
             </div>
-          </Col> */}
+          </Col>
         </Row>
       </div>
       
