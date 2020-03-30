68833f119b45fa7631f55b3c6077dafde383714c
pragyakarfuse <pragyakar@fusemachines.com>
2020-03-30 21:26:10 +0545

    Updated env with strapi env configs

--- src/@types/react-app-env.d.ts
+++ src/@types/react-app-env.d.ts
@@ -4,5 +4,9 @@ declare namespace NodeJS {
   interface ProcessEnv {
     REACT_APP_BASE_URL: string;
     REACT_APP_GOOGLE_ANALYTICS: string;
+    REACT_APP_GOOGLE_TRANSLATE_API_KEY: string;
+    REACT_APP_STRAPI_URL: string;
+    REACT_APP_STRAPI_USER: string;
+    REACT_APP_STRAPI_PASS: string;
   }
 }
--- src/config/index.ts
+++ src/config/index.ts
@@ -2,18 +2,33 @@ interface IApiEnvConfig {
   baseURL: string;
   timeout: number;
   googleAnalytics: string;
+  googleTranslateKey: string;
+  strapiURL: string;
+  strapiUser: string;
+  strapiPass: string;
 }
 
 interface IConfigValue {
   ApiEnv: IApiEnvConfig;
 }
 
-const { REACT_APP_BASE_URL, REACT_APP_GOOGLE_ANALYTICS } = process.env;
+const {
+  REACT_APP_BASE_URL,
+  REACT_APP_GOOGLE_ANALYTICS,
+  REACT_APP_GOOGLE_TRANSLATE_API_KEY,
+  REACT_APP_STRAPI_URL,
+  REACT_APP_STRAPI_USER,
+  REACT_APP_STRAPI_PASS
+} = process.env;
 
 const ApiEnv: IApiEnvConfig = {
   baseURL: REACT_APP_BASE_URL,
   timeout: 0,
-  googleAnalytics: REACT_APP_GOOGLE_ANALYTICS || 'UA-xxxxxxxxx-1'
+  googleAnalytics: REACT_APP_GOOGLE_ANALYTICS || "UA-xxxxxxxxx-1",
+  googleTranslateKey: REACT_APP_GOOGLE_TRANSLATE_API_KEY,
+  strapiURL: REACT_APP_STRAPI_URL,
+  strapiUser: REACT_APP_STRAPI_USER,
+  strapiPass: REACT_APP_STRAPI_PASS
 };
 
 const Config: IConfigValue = {
