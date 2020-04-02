55f5960ba2f4b8c955df2434416d8b077898ea3a
shrestharosy028 <rosy@fusemachines.com>
2020-04-02 21:58:43 +0545

    [FIX] : Correct tab names

--- src/routes/dashboard/supportFrontlineWorkers/SupportUsTabs/SupportUsTabs.tsx
+++ src/routes/dashboard/supportFrontlineWorkers/SupportUsTabs/SupportUsTabs.tsx
@@ -9,15 +9,15 @@ const SupportUsTabs = () => {
   return (
     <Row className="my-5 justify-content-center tab-container">
       <Col sm="12">
-        <Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
-          <Tab eventKey="home" title="Supporter">
+        <Tabs defaultActiveKey="supporters" id="uncontrolled-tab-example">
+          <Tab eventKey="supporters" title="Supporters">
             <SupportersTab />
           </Tab>
 
-          <Tab eventKey="requestForSupport" title="Request for Support">
+          <Tab eventKey="requestsForSupport" title="Requests for Support">
             <RequestorsTab />
           </Tab>
-          <Tab eventKey="requestFulfilled" title="Request Fulfilled">
+          <Tab eventKey="requestsFulfilled" title="Requests Fulfilled">
             <RequestsFulfilledTab />
           </Tab>
         </Tabs>
