2a9301b07f393b5fc849a816a1ea40c09df32a50
pragyakarfuse <pragyakar@fusemachines.com>
2020-04-03 19:15:35 +0545

    Removed strapi env configs

--- src/@types/react-app-env.d.ts
+++ src/@types/react-app-env.d.ts
@@ -5,8 +5,5 @@ declare namespace NodeJS {
     REACT_APP_BASE_URL: string;
     REACT_APP_GOOGLE_ANALYTICS: string;
     REACT_APP_GOOGLE_TRANSLATE_API_KEY: string;
-    REACT_APP_STRAPI_URL: string;
-    REACT_APP_STRAPI_USER: string;
-    REACT_APP_STRAPI_PASS: string;
   }
 }
