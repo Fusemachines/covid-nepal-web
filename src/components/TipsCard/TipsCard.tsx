03cc0dfccae2b1867a015058bcbeb7e90dfaf7c5
sanishkarmacharya <sanish@fusemachines.com>
2020-04-07 19:02:43 +0545

    css fixes in news page

--- src/components/TipsCard/TipsCard.tsx
+++ src/components/TipsCard/TipsCard.tsx
@@ -13,10 +13,10 @@ const TipsCard: React.FC<ITipsCardProps> = props => {
   const { title, createdAt, imageUrl, url } = props;
   return (
     <Media onClick={() => openUrl(url)}>
-      <div className="mr-2 align-self-center">
-        <img src={imageUrl} className="img-fluid" alt={title} style={{ width: "80px" }} />
+      <div className="mr-2 pt-1" style={{ width: "80px", overflow: "hidden"}}>
+        <img src={imageUrl} className="img-fluid" alt={title} style={{ width: "80px" }}/>
       </div>
-      <Media.Body className="align-self-center">
+      <Media.Body className="">
         <div className="font-weight-semibold">{title}</div>
         <div className="small-xs text-light">{createdAt}</div>
       </Media.Body>
