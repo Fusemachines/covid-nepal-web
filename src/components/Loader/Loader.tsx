ec3bd9f303d5d82490dfdfc16b6575ab3cbd4828
shrestharosy028 <rosy@fusemachines.com>
2020-04-08 17:27:05 +0545

    [FIX] : Create loader svg component

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LoaderIcon from '../Icons/LoaderIcon';
 
 interface ILoaderProps {
   block?: boolean;
@@ -6,23 +7,7 @@ interface ILoaderProps {
 
 const Loader: React.SFC<ILoaderProps> = ({ block }) => (
   <div className={`covid-loader ${block ? 'absolute' : ''}`}>
-    <svg width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="#fff">
-      <g fill="none" fillRule="evenodd">
-        <g transform="translate(1 1)" strokeWidth="2">
-          <circle strokeOpacity=".5" cx="18" cy="18" r="18" />
-          <path d="M36 18c0-9.94-8.06-18-18-18">
-            <animateTransform
-              attributeName="transform"
-              type="rotate"
-              from="0 18 18"
-              to="360 18 18"
-              dur="1s"
-              repeatCount="indefinite"
-            />
-          </path>
-        </g>
-      </g>
-    </svg>
+    <LoaderIcon />
   </div>
 );
 
