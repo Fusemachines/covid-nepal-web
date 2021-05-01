ef0563e0ad359f4b031fdced5f0993da32994f27
jagatmachines <jagatjyoti@fusemachines.com>
2021-05-01 22:36:29 +0545

    feat(covid-graph): substituting covid graph with twitter

--- src/routes/dashboard/landing/Landing.tsx
+++ src/routes/dashboard/landing/Landing.tsx
@@ -6,6 +6,7 @@ import CovidCases from "src/routes/dashboard/landing/covidCases";
 import TestingInformation from "src/routes/dashboard/landing/testingInfo";
 import LatestNews from "./latestNews";
 import Hospitals from "src/routes/dashboard/landing/hospitals";
+// import './johnhopkings.css'
 
 const Landing = () => {
   return (
@@ -19,7 +20,16 @@ const Landing = () => {
           <Col md="12" lg="3" className="mt-2 order-lg-first">
             <Scrollbars>
               {/* <div className="tweet-wrapper"> */}
-              <LatestNews />
+              {/* <LatestNews /> */}
+              <iframe
+                src="https://ourworldindata.org/explorers/coronavirus-data-explorer?zoomToSelection=true&hideControls=true&Metric=Confirmed+deaths&Interval=Cumulative&Relative+to+Population=false&Align+outbreaks=false&country=~NPL"
+                /* loading="lazy" */
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  border: 'none'
+                }}></iframe>
+
               {/* </div> */}
             </Scrollbars>
           </Col>
